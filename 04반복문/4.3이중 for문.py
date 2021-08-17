222dec47d5b67c33b9bc252523f0c37a75feafb7
dydwn <dydwn1335@gmail.com>
2021-08-17 21:34:40 +0900

    21:34 2021-08-17 아따 상당히 어려버부러

--- 04반복문/4.3이중 for문.py
+++ 04반복문/4.3이중 for문.py
@@ -9,3 +9,17 @@ for a in range(1, 10):
     for b in range(1, 10):
         print("%d x %d = %d" %(a, b, a*b))
     print("-" * 50)
+
+#C4-7 이중 for문으로 사각형 형태를 만들어라.
+
+for i in range(0, 11):
+    for j in range(0, 11):
+        print("ㅁ", end=" ")
+    print()
+
+#C4-8 이중 for문으로 역삼각형 형태의 숫자를 만들어라!
+
+for i in range(10, 0, -1):
+    for h in range(i):
+        print("%d" %i, end=" ")
+    print()
---
+++ 04반복문/4.5break문으로 빠져 나가기.py
@@ -0,0 +1,39 @@
+for i in range(1, 1001):
+    print(i)
+
+    if i == 10:
+        break
+
+#C4-9 while문으로 홀수의 누적 합계를 구하라.
+
+a = 1
+b = 0
+c = 0
+
+while a <= 100:
+    if a % 2 != 0:
+        b = b + a
+        print("%4d" %b, end=" ") # %d사이에 숫자를 넣으면 출력할 때 그 숫자만큼 자리를 할당함.
+
+
+    a = a + 1
+    if b % 10 == 0:
+        print()
+
+#C4-11 while문으로 영어 문장을 역순으로 출력하라!
+
+sentense = input("문장을 입력 하시오 : ")
+
+print(sentense[2])
+
+i = len(sentense) - 1
+
+print(i)
+
+while i >= 0:
+    if sentense[i] == " ":
+        print("-", end="")
+    else:
+        print("%s" % sentense[i], end="")
+
+        i = i - 1

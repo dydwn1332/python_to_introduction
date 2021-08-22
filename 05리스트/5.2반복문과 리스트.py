6d76f3201733ff73e97a8c254fefbfb0c0e9c453
dydwn <dydwn1335@gmail.com>
2021-08-22 23:10:57 +0900

    23:10 2021-08-22

--- 05리스트/5.2반복문과 리스트.py
+++ 05리스트/5.2반복문과 리스트.py
@@ -1,4 +1,24 @@
-﻿colors = ["빨간색", "파랑색", "초록색", "노랑색", "주황색"]
+﻿#5.2.1 for문에서 리스트 사용하기
+
+colors = ["빨간색", "파랑색", "초록색", "노랑색", "주황색"]
 
 for i in colors:
     print("나는 %s색을 좋아한다." %i)
+
+
+n = len(colors)
+
+for i in range(0, 5):
+    print("나는 %s색을 좋아한다." %colors[i])
+
+#5.2.2 while문에서 리스트 사용하기
+
+animal = ["사자", "호랑이", "토끼" , "강아지", "고양이"]
+
+i = 0
+
+while i < len(animal):
+    print(animal[i])
+
+    i = i + 1
+
---
+++ 05리스트/5.3리스트 요소 변환.py
@@ -0,0 +1,16 @@
+﻿#5.3.1 리스트 요소 수정하기
+
+flowers = ["목련", "벚꽃", "장미", "백일홍"]
+
+print(flowers)
+
+flowers[1] = "무궁화"
+print(flowers)
+
+#5.3.2 리스트 요소 추가하기
+ 
+arr = [5, 1, 2, 3, 4]
+print(arr)
+
+arr.append(10)
+print(arr)

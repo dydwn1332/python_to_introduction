ad2811da7ebad5bfce91a72c1f89088917661b9b
dydwn <dydwn1335@gmail.com>
2021-08-23 18:57:04 +0900

    123

--- 05리스트/5.3리스트 요소 변환.py
+++ 05리스트/5.3리스트 요소 변환.py
@@ -14,3 +14,36 @@ print(arr)
 
 arr.append(10)
 print(arr)
+
+scores = []
+
+while True:
+    score = int(input("성적을 입력하시오(종료 : -1) : "))
+
+    if score == -1:
+        break
+    else:
+        scores.append(score)
+
+print(scores)
+
+#5.3.3 리스트 요소 삽입하기
+
+fruits = ["apple", "orange", "banan", "cherry"]
+print(fruits)
+
+fruits.insert(1, "melon")
+print(fruits)
+
+fruits.insert(2, "strawberry")
+print(fruits)
+
+#5.3.4 리스트 요소 위치 찾기
+
+number = [1, 2, 3, 4, 5, 6, 7]
+print(number)
+
+idx = number.index(5)
+print(idx)
+
+#5.3.5 리스트 요소 삭제하기

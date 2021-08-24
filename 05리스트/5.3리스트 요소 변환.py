4d23d2d9becd92f8c7730d236ed192a56aadb290
dydwn <dydwn1335@gmail.com>
2021-08-24 21:57:16 +0900

    21:57 2021-08-24

--- 05리스트/5.3리스트 요소 변환.py
+++ 05리스트/5.3리스트 요소 변환.py
@@ -9,23 +9,23 @@ print(flowers)
 
 #5.3.2 리스트 요소 추가하기
  
-arr = [5, 1, 2, 3, 4]
-print(arr)
+#arr = [5, 1, 2, 3, 4]
+#print(arr)
 
-arr.append(10)
-print(arr)
+#arr.append(10)
+#print(arr)
 
-scores = []
+#scores = []
 
-while True:
-    score = int(input("성적을 입력하시오(종료 : -1) : "))
+#while true:
+#    score = int(input("성적을 입력하시오(종료 : -1) : "))
 
-    if score == -1:
-        break
-    else:
-        scores.append(score)
+#    if score == -1:
+#        break
+#    else:
+#        scores.append(score)
 
-print(scores)
+#print(scores)
 
 #5.3.3 리스트 요소 삽입하기
 
@@ -47,3 +47,53 @@ idx = number.index(5)
 print(idx)
 
 #5.3.5 리스트 요소 삭제하기
+
+member = ["홍지웅", 20, "경기도", "김포시", "dydwn1332@naver.com"]
+print(member)
+
+member.remove("홍지웅")
+print(member)
+
+data = [10, 20, 30, 40, 50, 60]
+print(data)
+
+x = data.pop(1)
+print(x)
+print(data)
+
+data.clear()
+print(data)
+
+#C5-1 1~20의 양의 정수 리스트를 생성하라!
+
+data = list(range(1, 21))
+
+print(data)
+
+for i in range(0, len(data)):
+    print("%d" %data[i], end=" ")
+   
+#5-2 짝수 번쨰 요소를 출력하라!
+
+print()
+for i in range(1, len(data) + 1):
+    if i % 2 == 0:
+        print(i, end=" ")
+
+    i = i + 1
+
+print()
+for i in range(1, len(data) + 1):
+    if i % 2 != 0:
+        print(i, end=" ")
+
+    i = i + 1
+
+#C5-4
+
+data = []
+
+for i in range(10, 21):
+    data.append(i)
+
+print(data)

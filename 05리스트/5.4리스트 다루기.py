f1801c29a414f5d8eccd7e0203e7f31e3cc16e7d
dydwn <dydwn1335@gmail.com>
2021-08-25 17:59:28 +0900

    test

--- 05리스트/5.4리스트 다루기.py
+++ 05리스트/5.4리스트 다루기.py
@@ -1,3 +1,51 @@
 ﻿#5.4.1 리스트 병합하기
 
-i
+person1 = [1, 2, 3]
+person2 = [4, 5, 6]
+
+person = person1 + person2
+
+print(person)
+
+#5.4.2 리스트 합계 구하기
+
+score = [80, 90, 95, 100]
+
+sm = sum(score)
+avg = sm / len(score)
+
+print("합계 : %d" %sm)
+print("평균 : %d" %avg)
+
+#5.4.3 리스트 순서 반대로 하기
+
+#리스트에서 요소들의 순서를 반대로 할 때에는 리스트위 reverse() 메소드를 이용한다.
+
+data = [10, 20, 30, 40, 50]
+print(data)
+
+data.reverse()
+print(data)
+#data.reverse()는 실행 결과에 나타난 것과 같이 리스트 요소들의 순서를 거꾸로 만든다.
+
+#5.4.4 리스트 복사하기
+
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+
+x = fruits.copy()
+print(x)
+
+b = fruits
+print(b)
+
+#5.4.5 리스트 정렬하기
+
+data = [1, 3, 5, 2, 4, 6, -1, -3]
+print(data)
+
+data.sort()
+print(data)
+
+data.sort(reverse=True)
+print(data)
---
+++ 05리스트/5.5문자열과 리스트.py
@@ -0,0 +1,15 @@
+#5.5.1 문자열 찾기
+
+string1 = "Python is fun!"
+print(string1)
+
+x = string1.find("fun")
+print(x)
+
+#5.5.2 문자열 치환하기
+
+string = "사과는 맛있다. 나는 사과를 좋아한다"
+print(string)
+
+string.replace("사과", "바나나")
+print(string)

ce77ccd16f6e00c869f2ed78258d6e2343337cf4
dydwn <dydwn1335@gmail.com>
2021-09-02 23:44:59 +0900

    23:44 2021-09-02

--- 02파이썬의 기본 문법/practice.py
+++ 02파이썬의 기본 문법/practice.py
@@ -5,3 +5,11 @@
 # a = str(input)
 # past = datetime.strptime(("%s", "%Y%m%d"), %a)
 # print(past)
+
+while True:
+    ip1 = input("숫자만 입력하시오 : ")
+    print(type(ip1))
+    ip2 = int(ip1)
+    print(type(ip2))
+    if type(ip2) == int:
+        break
--- 05리스트/5.5문자열과 리스트.py
+++ 05리스트/5.5문자열과 리스트.py
@@ -17,5 +17,61 @@ print(string)
 phone1 = "010-6295-3150"
 print(phone1)
 
-phone2 = phone1.replace("-", "")
+phone2 = phone1.replace("-", "")    #phone1에 replace메소드를 사용하여 "-"(하이픈)을 ""(NULL)로
+print(phone2)                       #변경 phone2는 하이픈이 없는 숫자만을 가지는 문자열이됨.
+
+#5.5.3 문자열 쪼개기
+
+hello = "have a nice day"
+print(hello)
+
+list1 = hello.split(" ")
+print(list1)
+print(type(list1))
+
+for i in range(0, len(list1)):
+    print("list1[%d] : %s" %(i, list1[i]))
+
+#5.5.4 리스트 문자열로 변환하기
+
+name = ["황예린", "홍지수", "안지영"]
+print(name)
+print(type(name))
+
+x =" ".join(name)
+print(x)
+print(type(x))
+
+phone1 = ["010", "6295", "3150"]
+print(phone1)
+
+phone2 = "".join(phone1)
 print(phone2)
+
+phone3 = int(phone2)
+print(phone3)
+
+#5.5.5 리스트 문자열에서 하이픈 삭제하기
+
+phone_list1 = ["010-1234-1234", "010-4321-4321", "010-6295-3150"]
+print(phone_list1)
+
+phone_list2 = []
+for i in phone_list1:
+    x = i.replace("-", "")
+
+    phone_list2.append(x)
+
+print(phone_list2)
+
+#5.5.6 리스트에서 문자열 치환하기
+
+sentence = ["Love me, love my dog", "No news is good news.",
+            "Blood is thicker than water"]
+print(sentence)
+# x = sentence.replace(" ", "")
+# print(x)
+
+for sentences in sentence:
+    x = sentences.replace(" ", "_")
+    print(x)

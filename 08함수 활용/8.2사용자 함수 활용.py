87bac98c585ba9390b9409227bee8157549a8628
dydwn <dydwn1335@gmail.com>
2021-09-27 15:07:27 +0900

    15:06 2021-09-27

--- 08함수 활용/8.2사용자 함수 활용.py
+++ 08함수 활용/8.2사용자 함수 활용.py
@@ -35,3 +35,53 @@ for i in range(1, a + 1):
         b = i * i * i
         sum = sum + b
         print("%d의 3제곱의 합 : %d, 총합계 : %d" %(i, b, sum))
+
+#8.2.3 회문인지 판별하기
+
+def is_palindrome(s):
+    for i in range(0, int(len(s)/2)):
+        if s[i] != s[len(s) - i - 1]:
+            return False
+
+    return True
+
+s = "rotatorr"
+for i in range(0, -6, -1):
+    print(s[i])
+if is_palindrome(s):
+    print("%s : 회문" %s)
+else:
+    print("%s : 회문아님" %s)
+
+#8.2.4 문장 단어 반대로 하기
+
+def reverse_sentence(s):
+    words = s.split(" ")
+    result = ""
+    for word in words:
+        result = word + " " + result
+
+    return result
+
+sentence = "nice to meet you"
+
+print(reverse_sentence(sentence))
+
+#8.2.5 문자열 존재여부 판별하기
+
+def string_find(a, b):
+    c = 0
+    words = a.split(" ")
+    for word in words:
+        if word == b:
+            c = c + 1
+    print("찾는 문장 : %s" %a)
+    print("찾는 단어 : %s" %b)
+    if c == 0:
+        print("찾지못했습니다.")
+    else:
+        print("%s를 %d건 찾았습니다." %(b, c))
+
+a = input("문장을 입력하시오 : ")
+b = input('찾을 단어를 입력하시오 : ')
+string_find(a, b)
